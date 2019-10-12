267747401f34ddb7ece583293f8e7d947af8d13a
Stanislav Ignatev <signatev@Stans-MacBook-Pro-13.local>
2019-10-11 18:23:36 -0700

    New Added

---
+++ libft/ft_lstdel.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
+{
+	t_list	*ptr;
+	t_list	*tmp;
+
+	ptr = *alst;
+	while (ptr)
+	{
+		tmp = ptr;
+		ft_lstdelone(&ptr, del);
+		ptr = tmp->next;
+	}
+	*alst = NULL;
+}
